1084d0e9a39a88b871f77161f3372526b877d7f7
samina <120310011+saminamushtaq@users.noreply.github.com>
2023-06-23 00:06:03 -0700

    Update Statistics_and_trends.py

--- Statistics_and_trends.py
+++ Statistics_and_trends.py
@@ -119,9 +119,119 @@ plt.savefig("UAE")
 plt.show()
 
 
+#getting usa urban population data
+df_usa = df_countries[df_countries["Country Name"] == "United States"]
+df_urban = df_usa[df_usa['Indicator Name']=="Urban population (% of total population)"].transpose()
+df_urban = df_urban.drop(["Country Name", "Indicator Name"])
+
+# drop nan values
+df_urban = df_urban.dropna().transpose()
+
+#Creating dataframes for analysis
+USA = pd.DataFrame()
+USA["Urban Population"] = df_urban.iloc[0]
+USA["Urban Population"] = pd.to_numeric(USA["Urban Population"])
+
+#getting usa agricultural data
+df_agricultural = df_usa[df_usa["Indicator Name"] == "Agricultural land (% of land area)"].transpose()
+df_agricultural = df_agricultural.drop(["Country Name", "Indicator Name"])
+
+# drop nan values
+df_agricultural = df_agricultural.dropna()
+df_agricultural = df_agricultural.transpose()
 
 
+#Creating dataframes for analysis
+USA["Agricultural land"] = df_agricultural.iloc[0]
+USA["Agricultural land"] = pd.to_numeric(USA["Agricultural land"])
+USA = USA.dropna()
+
+#getting usa arabel land data
+Arable = df_usa[df_usa["Indicator Name"] == "Arable land (% of land area)"].transpose()
+Arable = Arable.drop(["Country Name", "Indicator Name"])
+
+# drop nan values
+Arable = Arable.dropna().transpose()
+
+#Creating dataframes for analysis
+USA["Arable land"] = Arable.iloc[0]
+USA["Arable land"] = pd.to_numeric(USA["Arable land"])
+USA = USA.dropna()
+
+#Reseting the index column from years 
+USA = USA.reset_index(level=0)
+USA = USA.rename(columns = {"index":"Year"})
+
+#Convert year to datetime object
+USA["Year"] = pd.to_datetime(USA["Year"])
+
+#Applying statistical tools to the data frame
+print("Covariance")
+print(USA.cov())
+print("Correlation")
+corr_usa = USA.corr()
+print(corr_usa)
+
+#Line plot
+plt.figure()
+plt.plot(USA["Year"], USA["Urban Population"], 
+            label = "Urban Population")
+plt.plot(USA["Year"], USA["Agricultural land"], 
+            label = "Agricultural land")
+plt.plot(USA["Year"], USA["Arable land"], 
+            label = "Arable land")
+plt.xlabel("Year")
+plt.title("USA")
+plt.legend(loc="lower right")
+plt.savefig("USA")
+plt.show()
 
 
+#getting australia data for co2 indicator
+df_co2 = df_countries[df_countries["Indicator Name"] == "CO2 emissions (kt)"]
+df_aus = df_co2[df_co2["Country Name"] == "Australia"].transpose()
+df_aus = df_aus.drop(["Country Name", "Indicator Name"])
+
+# drop nan values
+df_aus = df_aus.dropna()
+df_aus = df_aus.transpose()
 
 
+#Creating dataframes for analysis
+CO2 = pd.DataFrame()
+CO2["Australia"] = df_aus.iloc[0]
+
+#getting bangladesh data for co2 indicator
+df_bgd = df_co2[df_co2["Country Name"] == "Bangladesh"].transpose()
+df_bgd = df_bgd.drop(["Country Name", "Indicator Name"])
+
+# drop nan values
+df_bgd = df_bgd.dropna()
+df_bgd = df_bgd.transpose()
+
+
+#Creating dataframes for analysis
+CO2["Bangladesh"] = df_bgd.iloc[0]
+
+#getting pakistan data for co2 indicator
+df_pak = df_co2[df_co2["Country Name"] == "Pakistan"].transpose()
+df_pak = df_pak.drop(["Country Name", "Indicator Name"])
+
+# drop nan values
+df_pak = df_pak.dropna()
+df_pak = df_pak.transpose()
+
+
+#Creating dataframes for analysis
+CO2["Pakistan"] = df_pak.iloc[0]
+
+#getting canada data for co2 indicator
+df_cda = df_co2[df_co2["Country Name"] == "Canada"].transpose()
+df_cda = df_cda.drop(["Country Name", "Indicator Name"])
+
+# drop nan values
+df_cda = df_cda.dropna()
+df_cda = df_cda.transpose()
+
+#Creating dataframes for analysis
+CO2["Canada"] = df_cda.iloc[0]
